0207a4ef2cfb04dee6ad6817b3dca40b04d46d1e
n.gorenok <n.gorenok@gmail.com>
2018-01-08 01:44:41 +0300

    changing App

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 "use strict";
-
+require('es6-promise').polyfill();
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import ForumBlock from './components/ForumBlock';
 
-let titleText='Лучшие темы!';
+
+let titleText='The best Topics!';
+let messagesArray=[{header:'', message:'', code:0}];
 
 ReactDOM.render(
     <ForumBlock
         title={titleText}
-        startWorkMode={1}
+        historyMessages={messagesArray}
     />
     , document.getElementById('container')
 );
