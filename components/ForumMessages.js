a448708d15ba05f976f88de454c2ccb563506b19
n.gorenok <n.gorenok@gmail.com>
2018-01-08 01:47:03 +0300

    changing ForumMessages

--- components/ForumMessages.js
+++ components/ForumMessages.js
@@ -1,43 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import './ForumMessages.css';
 
 
-
-class ForumMessages extends React.Component {
+class ForumMessages extends React.PureComponent {
 
     static propTypes = {
-        messageText: PropTypes.string.isRequired,
+        newHeader: PropTypes.string.isRequired,
+        newMessage: PropTypes.string.isRequired,
         cbHeaderTextChanged: PropTypes.func.isRequired,
         cbMessageTextChange: PropTypes.func.isRequired,
-        //code: PropTypes.number.isRequired,
         cbSendClicked: PropTypes.func.isRequired,
     };
     headerTextChange = (EO) => {
         console.log('HeaderMessage: текст свободного ввода заголовка изменён - '+EO.target.value);
         this.props.cbHeaderTextChanged(EO.target.value);
-    }
+    };
 
     messageTextChange = (EO) => {
         console.log('TextMessage: текст свободного ввода текста изменён - '+EO.target.value);
         this.props.cbMessageTextChange(EO.target.value);
-    }
+    };
 
-    sendButton = (EO) => {
+    sendButton = () => {
         console.log('Кнопка send нажата - ');
         this.props.cbSendClicked();
-    }
+    };
 
     render() {
+        console.log('Render ForumMessages');
         return (
             <div className='ForumMessages'>
-                <form className="formForum">
-                    <label htmlFor="header" className="headerMessage">Header</label>
-                    <input type="text" onChange={this.headerTextChange} id="header" className="header-control" placeholder="Header" required autoFocus />
-                    <label htmlFor="message" className="textMessage">Message</label>
-                    <textarea id="message" className="message-control" defaultValue={this.props.messageText} onChange={this.messageTextChange} placeholder="Type your message" maxLength="140" rows="7"/>
-                    <button className="sendButton" onClick={this.sendButton} type="button" >Send</button>
+                <form className='FormForum'>
+                    <div className='InputBlock'>
+                        <label htmlFor='header' className='HeaderMessage'>Header</label>
+                        <input type='text' onChange={this.headerTextChange} id='header' className='Header-control' defaultValue={this.props.newHeader} placeholder="Type header" required autoFocus />
+                    </div>
+                    <div className='InputBlock'>
+                        <label htmlFor='message' className='TextMessage'>Message</label>
+                        <textarea id='message' className='Message-control' defaultValue={this.props.newMessage} onChange={this.messageTextChange} placeholder='Type your message' maxLength='140' rows='7'/>
+                    </div>
+                    <button className='SendButton' onClick={this.sendButton} type='button' >Send</button>
                 </form>
             </div>
         )
